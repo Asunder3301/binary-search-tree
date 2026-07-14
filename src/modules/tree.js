7238af1b53bfc9a045fcd43de3a616ae6cfcdac5
asunder <trashemail3301@gmail.com>
2026-07-14 12:30:41 -0400

    Recursively build a BST

--- src/modules/tree.js
+++ src/modules/tree.js
@@ -4,4 +4,18 @@ export class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
   }
+
+  buildTree(array) {
+    if (array.length === 0) {
+      return null;
+    }
+
+    let mid = Math.floor(array.length / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
 }
