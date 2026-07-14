32ed09651376ee46569f06a8a4866f8403dccb5b
asunder <trashemail3301@gmail.com>
2026-07-14 12:31:45 -0400

    Add merge sort method that eliminates duplicates
    Duplicates are removed to avoid balancing
    complexities.

--- src/modules/tree.js
+++ src/modules/tree.js
@@ -5,6 +5,38 @@ export class Tree {
     this.root = this.buildTree(array);
   }
 
+  mergeSort(array) {
+    if (array.length <= 1) return array;
+
+    const mid = Math.floor(array.length / 2);
+
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+
+    return this.merge(this.mergeSort(left), this.mergeSort(right));
+  }
+
+  merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else if (left[leftIndex] > right[rightIndex]) {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      } else {
+        //Handle duplicates: push only one and ignore other
+        result.push(left[leftIndex]);
+        leftIndex++;
+        rightIndex++;
+      }
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
+
   buildTree(array) {
     if (array.length === 0) {
       return null;
